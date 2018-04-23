f34108ce12ab21c0f54d2ee696cea6b72637e1b2
crumpledstudio <pedro@urbans.mobi>
2018-04-23 09:07:14 +0100

    Template Try #2

--- routes/main.js
+++ routes/main.js
@@ -1,15 +1,50 @@
 const router = require('express').Router();
+const async = require('async');
+const Gig = require('../models/gigs');
+const User = require('../models/user');
+
+
 
 router.get('/', (req, res, next) => {
     res.render('main/home');
 });
 
 router.get('/my-gigs', (req, res, next) => {
-    res.render('main/my-gigs');
+    Gig.find({ owner: req.user._id}, function(err, gigs) {
+        res.render('main/my-gigs', { gigs: gigs });
+    })
 });
 
-router.route('add-new-gig').get((req, res, next) => {
-    res.render('main/add-new-gig');
+router.route('/add-new-gig')
+    .get((req, res, next) => {
+        res.render('main/add-new-gig');
+    })
+    .post((req, res, next) => {
+        async.waterfall([
+            function(callback) {
+                var gig = new Gig();
+                gig.owner = req.user._id;
+                gig.title = req.body.gig_title;
+                gig.category = req.body.gig_category;
+                gig.about = req.body.gig_about;
+                gig.price = req.body.gig_price;
+                gig.save(function(err) {
+                    callback(err, gig);
+                });
+            },
+
+            function(gig, callback) {
+                User.update(
+                    {
+                        _id: req.user_id
+                    },{
+                        $push: { gigs: gig._id }
+                    }, function(err, count) {
+                        res.redirect('/my-gigs');
+                    }
+            )
+        }
+    ]);
 });
 
 
